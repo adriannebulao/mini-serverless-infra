1cc2227f4bce552291c9d17921112b08c981b35f
adriannebulao <amcbulao@gmail.com>
2025-07-27 16:49:36 +0800

    feat: deploy frontend to s3

--- lib/mini-serverless-backend-stack.ts
+++ lib/mini-serverless-backend-stack.ts
@@ -7,14 +7,21 @@ import { HttpMethod } from "aws-cdk-lib/aws-apigatewayv2";
 import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
 import path from "path";
 import { config } from "dotenv";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 config();
 
 export class MiniServerlessBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const tableName = process.env.TABLE_NAME;
+    if (!tableName) {
+      throw new Error("TABLE_NAME environment variable is not defined.");
+    }
+
     const table = new dynamodb.Table(this, "AppTable", {
-      tableName: "EmployeeProjectTable",
+      tableName: tableName,
       partitionKey: { name: "PK", type: dynamodb.AttributeType.STRING },
       sortKey: { name: "SK", type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
@@ -59,18 +66,54 @@ export class MiniServerlessBackendStack extends cdk.Stack {
     table.grantReadWriteData(projectFn);
     table.grantReadWriteData(assignmentFn);
 
+    const frontendUrl = process.env.FRONTEND_URL;
+    if (!frontendUrl) {
+      throw new Error("FRONTEND_URL environment variable is not defined.");
+    }
+
     const api = new apigwv2.HttpApi(this, "HttpApi", {
       apiName: "MiniProjAPI",
       corsPreflight: {
-        allowHeaders: ["Content-Type"],
+        allowHeaders: ["Content-Type", "Authorization"],
         allowMethods: [
           apigwv2.CorsHttpMethod.GET,
           apigwv2.CorsHttpMethod.POST,
           apigwv2.CorsHttpMethod.PUT,
           apigwv2.CorsHttpMethod.DELETE,
+          apigwv2.CorsHttpMethod.OPTIONS,
         ],
-        allowOrigins: ["http://localhost:3000"],
+        allowOrigins: [frontendUrl],
+        maxAge: cdk.Duration.days(10),
       },
+    });
+
+    const frontendBucket = new s3.Bucket(this, "FrontendBucket", {
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "index.html",
+      publicReadAccess: true,
+      blockPublicAccess: {
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    const frontendBuildPath = process.env.FRONTEND_BUILD_PATH;
+    if (!frontendBuildPath) {
+      throw new Error(
+        "FRONTEND_BUILD_PATH environment variable is not defined."
+      );
+    }
+    new BucketDeployment(this, "FrontendDeployment", {
+      sources: [Source.asset(frontendBuildPath)],
+      destinationBucket: frontendBucket,
+    });
+
+    new cdk.CfnOutput(this, "FrontendURL", {
+      value: frontendBucket.bucketWebsiteUrl,
     });
 
     const employeeIntegration = new HttpLambdaIntegration(
